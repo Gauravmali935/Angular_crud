ac8c9edf54a43f34c38f006245d7e984bf32e182
Gauravmali935 <gauravmali935@gmail.com>
2025-09-29 18:13:14 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { Form, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { UserStore } from './store/user-store';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,7 @@ export class AppComponent {
   title = 'angular_crud_UI';
   formBuilder = inject(FormBuilder);
   userForm!: FormGroup;
+  userStore : UserStore
   constructor() {
     this.userForm = this.formBuilder.group({
       name: [''],
@@ -19,11 +21,14 @@ export class AppComponent {
       age : [''],
       gender: ['']
     });
+    this.userStore = new UserStore();
     }
     save()
     {
       let userdata = this.userForm.value;
       console.log(userdata);
+      this.userStore.addUser(userdata);
+      this.userForm.reset();
     }
     clearForm()
     {
